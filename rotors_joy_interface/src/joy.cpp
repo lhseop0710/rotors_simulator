559b0672b4c014579fc68c77ba10c305424d703a
Hoseop.Lee <79184520+lhseop0710@users.noreply.github.com>
2021-07-06 00:20:50 +0900

    Update joy.cpp
    XBOX360 controller version

--- rotors_joy_interface/src/joy.cpp
+++ rotors_joy_interface/src/joy.cpp
@@ -10,7 +10,6 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,30 +36,32 @@ Joy::Joy() {
   control_msg_.thrust.z = 0;
   current_yaw_vel_ = 0;
 
-  pnh.param("axis_roll_", axes_.roll, 0);
-  pnh.param("axis_pitch_", axes_.pitch, 1);
-  pnh.param("axis_thrust_", axes_.thrust, 2);
+  pnh.param("axis_roll_", axes_.roll, 2);
+  pnh.param("axis_pitch_", axes_.pitch, 3);
+  pnh.param("axis_yaw_", axes_.yaw, 0);
+  pnh.param("axis_thrust_", axes_.thrust, 1);
 
   pnh.param("axis_direction_roll", axes_.roll_direction, -1);
   pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
+  pnh.param("axis_thrust_yaw", axes_.yaw_direction, 1);
   pnh.param("axis_direction_thrust", axes_.thrust_direction, 1);
 
-  pnh.param("max_v_xy", max_.v_xy, 1.0);  // [m/s]
-  pnh.param("max_roll", max_.roll, 10.0 * M_PI / 180.0);  // [rad]
-  pnh.param("max_pitch", max_.pitch, 10.0 * M_PI / 180.0);  // [rad]
-  pnh.param("max_yaw_rate", max_.rate_yaw, 45.0 * M_PI / 180.0);  // [rad/s]
+  pnh.param("max_v_xy", max_.v_xy, 3.0);  // [m/s]
+  pnh.param("max_roll", max_.roll, 45.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_pitch", max_.pitch, 45.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_yaw_rate", max_.rate_yaw, 300.0 * M_PI / 180.0);  // [rad/s]
   pnh.param("max_thrust", max_.thrust, 30.0);  // [N]
 
   pnh.param("v_yaw_step", v_yaw_step_, 0.05);  // [rad/s]
 
   pnh.param("is_fixed_wing", is_fixed_wing_, false);
 
-  pnh.param("button_yaw_left_", buttons_.yaw_left, 3);
-  pnh.param("button_yaw_right_", buttons_.yaw_right, 4);
-  pnh.param("button_ctrl_enable_", buttons_.ctrl_enable, 5);
-  pnh.param("button_ctrl_mode_", buttons_.ctrl_mode, 10);
-  pnh.param("button_takeoff_", buttons_.takeoff, 7);
-  pnh.param("button_land_", buttons_.land, 8);
+  pnh.param("button_yaw_left_", buttons_.yaw_left, 4);
+  pnh.param("button_yaw_right_", buttons_.yaw_right, 5);
+  pnh.param("button_ctrl_enable_", buttons_.ctrl_enable, 2);
+  pnh.param("button_ctrl_mode_", buttons_.ctrl_mode, 1);
+  pnh.param("button_takeoff_", buttons_.takeoff, 3);
+  pnh.param("button_land_", buttons_.land, 0);
 
   namespace_ = nh_.getNamespace();
   joy_sub_ = nh_.subscribe("joy", 10, &Joy::JoyCallback, this);
@@ -79,17 +80,19 @@ void Joy::JoyCallback(const sensor_msgs::JoyConstPtr& msg) {
   current_joy_ = *msg;
   control_msg_.roll = msg->axes[axes_.roll] * max_.roll * axes_.roll_direction;
   control_msg_.pitch = msg->axes[axes_.pitch] * max_.pitch * axes_.pitch_direction;
+  control_msg_.yaw_rate = msg->axes[axes_.yaw] * max_.rate_yaw * axes_.yaw_direction;
+  
+  //if (msg->buttons[buttons_.yaw_left]) {
+    //current_yaw_vel_ = max_.rate_yaw;
+  //}
+  //else if (msg->buttons[buttons_.yaw_right]) {
+    //current_yaw_vel_ = -max_.rate_yaw;
+  //}
+  //else {
+    //current_yaw_vel_ = 0;
+  //}
+  //control_msg_.yaw_rate = current_yaw_vel_;
 
-  if (msg->buttons[buttons_.yaw_left]) {
-    current_yaw_vel_ = max_.rate_yaw;
-  }
-  else if (msg->buttons[buttons_.yaw_right]) {
-    current_yaw_vel_ = -max_.rate_yaw;
-  }
-  else {
-    current_yaw_vel_ = 0;
-  }
-  control_msg_.yaw_rate = current_yaw_vel_;
 
   if (is_fixed_wing_) {
     double thrust = msg->axes[axes_.thrust] * axes_.thrust_direction;
